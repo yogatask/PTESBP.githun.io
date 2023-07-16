b0e2509461e77a45731227f122295d346794d71a
yogatask <139001658+yogatask@users.noreply.github.com>
2023-07-16 17:26:01 +0530

    My School

---
+++ js/slide.js
@@ -0,0 +1,13 @@
+ 
+        <script>
+         wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+        </script>
